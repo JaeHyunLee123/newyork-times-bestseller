624ce94278d3f1e5976c542bfac4d070cb6c24f7
JaeHyunLee123 <jhyon123@gmail.com>
2023-06-23 11:53:14 +0900

    feat: list.[id] pages

--- pages/list/[id].js
+++ pages/list/[id].js
@@ -1,9 +1,44 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Detail() {
+  const [listInfo, setListInfo] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
   const {
     query: { id },
   } = useRouter();
 
-  return <div>{id}</div>;
+  useEffect(() => {
+    if (id) {
+      (async function () {
+        const { results } = await (await fetch(`/api/list/${id}`)).json();
+        setListInfo(results);
+        setIsLoading(false);
+      })();
+    }
+  }, [id]);
+
+  return (
+    <div>
+      {isLoading ? (
+        <h2>Loading</h2>
+      ) : (
+        <>
+          <h1>{listInfo.list_name}</h1>
+          {listInfo.books.map((book) => (
+            <div key={book.primary_isbn13}>
+              <img src={`${book.book_image}`} />
+              <span>{book.title}</span>
+              <span>{book.author}</span>
+              <Link href={`${book.amazon_product_url}`}>
+                <span>{`Buy now ->`}</span>
+              </Link>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
